322a6e6501728ed3b235026e4259acce1174fd3f
vichetch <ukvichetch@gmail.com>
2021-12-30 17:40:19 +0700

    update metahead

--- reacts/components/headMeta/index.jsx
+++ reacts/components/headMeta/index.jsx
@@ -3,7 +3,7 @@ export const MetaViewPort = ({ themeColor }) => {
     <>
       <meta name="robots" content="index, follow"></meta>
       <meta charSet="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content={themeColor} />
     </>
